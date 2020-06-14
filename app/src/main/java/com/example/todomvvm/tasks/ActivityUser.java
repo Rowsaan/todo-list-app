f3da50246b21d672fbd5e8964ab81e16846799e5
unknown <bravehunkdead@gmail.com>
2020-06-14 13:25:08 +0545

    first commit

---
+++ app/src/main/java/com/example/todomvvm/tasks/ActivityUser.java
@@ -0,0 +1,16 @@
+package com.example.todomvvm.tasks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.todomvvm.R;
+
+public class ActivityUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
